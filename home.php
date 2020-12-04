8a6cc28bacfe122a359313fbed594b990e16f2b4
NilsC <60584997+Aqualio@users.noreply.github.com>
2020-12-04 06:37:39 +0000

    Add files via upload

---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+		<met name="viewport" content="width=device-width,
+		initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="./css/home.css">
+        <script src="./js/home.js"></script>
+        <title>Surfrider Web</title>
+    </head>
+    <body>
+        <nav id="menuBar" class="bar-container flex-wraper">
+            <a href="home.php"><li><img id="logo" src="./media/astolfo_plushe.png" alt=""></li></a>
+            <ul id="navMenu">
+                <li><a href="home.php">Home</a></li>
+                <li><a href="team.php">The team</a></li>
+            </ul>
+        </nav>
+
+        <section>
+            <div id="presentation">
+                <p>The sport. The big waves. The dreamy beaches. Surfing has still to be dethroned.  However, the behind-the-scenes are more than concerning.
+
+<br>Otitis, gastroenteritis, skin irritations. Three diseases that one over three surfers confirmed having it after only one session.  All year long, the water is infected by two types of pollution : bacteriological and chemical. With that, there's also the trash. They are everywhere. More than the view, they can have a dangerous physical impact on the surfers ( the person who wrote this text in french almost got hit by a toilet bowl! )
+
+To act again such risks, Surfrider wishes for the littorals to be more monitored all year long. Nowadays, it's only done in summer as it got imposes by the UE. To back up our claims, we need datas about the practice of the surfers, their frequency, their exposure times and their observation of a given place.
+                </p>
+            </div>
+            <div id="imgSurfer">
+                <img src=".\media\Surfer.png" alt="Surfer">
+            </div>
+        </section>
+
+        <p style="white-space: pre-wrap; min-height: 1.5em; text-align: left;"> </p>
+        
+        <section>
+            <div id="downloadtext">
+                <p>Join the community & Help your fellow surfers</p>
+            </div>
+
+            <div id="download">
+                <p>DOWNLOAD NOW</p>
+                <a href="home.php"><img src="./media/app_store_dl_button.png" alt=""></a>
+                <a href="home.php"><img id="gglplay" src="./media/google_play_btn.png" alt=""></a>
+            </div>
+            
+        </section>
+
+        <p style="white-space: pre-wrap; min-height: 1.5em; text-align: left;"> </p>
+        
+        <footer>
+            <p>NUIT DE L'INFO</p>
+        </footer>
+    </body>
+</html>
